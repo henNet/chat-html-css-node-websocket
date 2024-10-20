d67e34d2903ba4923e4098335cbd0d42c383231f
henNet <dmf.n123@gmail.com>
2024-10-20 20:59:54 -0300

    feat: modicicado para wss

--- frontend/script.js
+++ frontend/script.js
@@ -25,7 +25,7 @@ function conectar() {
 
   // clienteSocket = new WebSocket("http://localhost:3000");
   clienteSocket = new WebSocket(
-    "ws://backend-chat-html-css-node-websocket.vercel.app/"
+    "wss://backend-chat-html-css-node-websocket.vercel.app/"
   );
 
   let msg = {
